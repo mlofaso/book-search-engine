56ca302325eee5e79edb01c5e66c0f54c0a66553
Martin LoFaso <marty@Martins-Air.attlocal.net>
2024-05-22 18:32:23 -0700

    added typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,46 @@
+const typeDefs = `
+type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+}
+
+type Book {
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+
+type Auth {
+    token: ID
+    user: String
+}
+
+input BookData {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+
+type Query {
+    me: User
+}
+
+type Mutation {
+    addUser(name: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth 
+    saveBook(bookData: BookData!): User
+    removeBook(bookId: ID): User
+}
+
+`;
+
+module.exports = typeDefs;
